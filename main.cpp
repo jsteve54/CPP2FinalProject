1d48597163d30629c9af19a3a6306c338978d898
cluu2 <cluu2@uncc.edu>
2018-04-29 09:43:00 -0400

    Edited formatting

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void buildDeck(vector <Card> d) {
+void buildDeck(vector <card> d) {
     d.push_back(Card ("Spades", 0, 1));
     d.push_back(Card ("Spades", 1, 2));
     d.push_back(Card ("Spades", 2, 3));
@@ -19,7 +19,7 @@ void buildDeck(vector <Card> d) {
     d.push_back(Card ("Spades", 10, 10));
     d.push_back(Card ("Spades", 11, 10));
     d.push_back(Card ("Spades", 12, 10));
-    
+
     d.push_back(Card ("Clubs", 0, 1));
     d.push_back(Card ("Clubs", 1, 2));
     d.push_back(Card ("Clubs", 2, 3));
@@ -33,7 +33,7 @@ void buildDeck(vector <Card> d) {
     d.push_back(Card ("Clubs", 10, 10));
     d.push_back(Card ("Clubs", 11, 10));
     d.push_back(Card ("Clubs", 12, 10));
-    
+
     d.push_back(Card ("Diamonds", 0, 1));
     d.push_back(Card ("Diamonds", 1, 2));
     d.push_back(Card ("Diamonds", 2, 3));
@@ -47,7 +47,7 @@ void buildDeck(vector <Card> d) {
     d.push_back(Card ("Diamonds", 10, 10));
     d.push_back(Card ("Diamonds", 11, 10));
     d.push_back(Card ("Diamonds", 12, 10));
-    
+
     d.push_back(Card ("Hearts", 0, 1));
     d.push_back(Card ("Hearts", 1, 2));
     d.push_back(Card ("Hearts", 2, 3));
@@ -63,12 +63,12 @@ void buildDeck(vector <Card> d) {
     d.push_back(Card ("Hearts", 12, 10));
 }
 
+
 int main() {
 	//INSERT CARDS IN DECK CLASS
 	vector <Card> deck;
 	    
 	buildDeck(deck);
-
 
 	return 0;
 }
