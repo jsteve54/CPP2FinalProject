c2c88f18f86453f8c4bb078922d9c290e8de1ca5
cluu2 <cluu2@uncc.edu>
2018-04-30 00:14:18 -0400

    Changed various things. Added card dealing.
    card.h
    Added printCard member function
    
    card.cpp
    Added printCard function
    
    main.cpp
    Changed values of cards to correct values
    Added dealCards function
    Added testing code to print out what is left in the deck, player hand, and computer hand

--- card.cpp
+++ card.cpp
@@ -1,10 +1,30 @@
 #include "card.h"
 #include <math.h>
-#include <string>
+#include <sstream>
 #include <iostream>
 
 Card::Card (string s, int v, int pv) {
     suit = s;
     value = v;
     pointValue = pv;
+    name = "";
+}
+
+void Card::printCard () {
+    ostringstream convert;
+    
+    if (value == 1)
+        name = "Ace";
+    else if (value == 11)
+        name = "Jack";
+    else if (value == 12)
+        name = "Queen";
+    else if (value == 13)
+        name = "King";
+    else {
+        convert << value;
+        name = convert.str();
+    }
+        
+    cout << name << " of " << suit << endl;
 }
